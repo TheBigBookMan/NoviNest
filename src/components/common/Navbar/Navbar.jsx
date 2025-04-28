8b57223bbf72fff1cefe280e01bb0f402e65d92a
Ben Smerd <bsmerd@adam.com.au>
2025-04-28 22:20:17 +0930

    update navbar with the dropdown

--- src/components/common/Navbar/Navbar.jsx
+++ src/components/common/Navbar/Navbar.jsx
@@ -5,7 +5,7 @@ import DesktopNav from './DesktopNav';
 
 const Navbar = () => {
     return (
-        <div className="h-[80px] w-full flex  border-b sticky top-0 z-50 bg-white px-6 items-center ">
+        <div className="h-[80px] w-full flex  border-b sticky top-0 z-50 px-6 items-center bg-[#858D7E]">
             <DesktopNav />
 
             <MobileNav />
