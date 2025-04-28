864c2fe0b630dfe9a19ded5e267ac136de41d319
Ben Smerd <bsmerd@adam.com.au>
2025-04-29 07:08:03 +0930

    add in more information and update styling

--- src/components/features/HomePage/Info.jsx
+++ src/components/features/HomePage/Info.jsx
@@ -1,9 +1,17 @@
 const Info = () => {
     return (
-        <div className='flex flex-col h-screen'>
-            <p>Welcome to Novi Nest - Where Understanding Nurtures Growth</p>
+        <div className='flex flex-col gap-8 h-screen -mt-12'>
+            <p className='text-lg font-cinzel font-bold'>Welcome to Novi Nest - Where Understanding Nurtures Growth</p>
 
-            <p>At Novi Nest Psychology, we help families and educators gain meaningful insight into the developmental profiles of children in their care. Through evidence-based assessments, we aim to highlight each child's strengths while identifying the support they need to thrive. We believe assessments should be accessible to all. That's why, where possible, we conduct school-based assessments, allowing children to be seen in a familiar environment rather than a clinical setting. By reducing stress and increasing comfort, we ensure assessment outcomes that reflect your child's natural abilities and learning needs. Our reports go beyond numbers—they provide clear, actionable recommendations that empower families and educators to support each child's development. At Novi Nest, our goal is simple: to foster understanding, unlock potential, and inform the way forward.</p>
+            <div className='flex flex-col gap-4 text-sm'>
+
+                <p className='leading-relaxed'>At Novi Nest Psychology, we help families and educators gain meaningful insight into the developmental profiles of children in their care. Through evidence-based assessments, we aim to highlight each child's strengths while identifying the support they need to thrive.</p>
+
+                <p className='leading-relaxed'>We believe assessments should be accessible to all. That's why, where possible, we conduct school-based assessments, allowing children to be seen in a familiar environment rather than a clinical setting. By reducing stress and increasing comfort, we ensure assessment outcomes that reflect your child's natural abilities and learning needs.</p>
+                
+                <p className='leading-relaxed'>Our reports go beyond numbers—they provide clear, actionable recommendations that empower families and educators to support each child's development. At Novi Nest, our goal is simple: to foster understanding, unlock potential, and inform the way forward.</p>
+
+            </div>
         </div>
     )
 }
