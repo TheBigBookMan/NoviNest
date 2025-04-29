a4a2e4ef06292b6dbd68dbd700f32a0f5dfb6265
Ben Smerd <bsmerd@adam.com.au>
2025-04-30 07:23:15 +0930

    update styling of footer

--- src/components/common/Footer/Footer.jsx
+++ src/components/common/Footer/Footer.jsx
@@ -4,11 +4,11 @@ import LgbtFlag from '../../../assets/lgbt-flag.png';
 
 const Footer = () => {
     return (
-        <div className='flex flex-col md:flex-row h-[80px]'>
+        <div className='flex flex-col md:flex-row h-fit  text-sm p-4 bg-[#858D7E] text-slate-100'>
             <p>Novi Nest acknowledges the Traditional Custodians of the land on which we work, the Yuggera and Turrbal peoples. We pay our deepest respect to elders past, present and emerging. 
             We are a proudly inclusive organisation and an ally of the LGBTIQ+ community and the movement toward equality.</p>
 
-            <div className='flex  gap-1'>
+            <div className='flex justify-center gap-1'>
                 <img src={AboriginalFlag} alt='Indigenous Australian flag' className='w-12 md:w-40' />
                 <img src={TorresFlag} alt='Torres Strait Islander flag' className='w-12 md:w-40' />
                 <img src={LgbtFlag} alt='LGBTQ+ Progress Pride flag' className='w-12 md:w-40' />
@@ -16,7 +16,7 @@ const Footer = () => {
 
             <p>©2025 Novi Nest</p>
 
-            <p>Made by Ben Smerd: <a className='underline text-blue-500' href="mailto:bjsmerd@gmail.com">bjsmerd@gmail.com</a></p>
+            <p>Made by Ben Smerd: <a className='underline text-blue-100 ' href="mailto:bjsmerd@gmail.com">bjsmerd@gmail.com</a></p>
         </div>
     )
 }
