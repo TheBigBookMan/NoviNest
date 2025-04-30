818a2c78214149b04b78e66d9d6b0447a092f74b
Ben Smerd <bsmerd@adam.com.au>
2025-05-01 07:48:34 +0930

    create the fees page

--- src/pages/Fees.jsx
+++ src/pages/Fees.jsx
@@ -1,13 +1,22 @@
-
-
-//  https://entangledminds.com.au/rates-insurance
-// have link to and from the services page
+import Contact from "../components/common/Contact/Contact";
+import Footer from "../components/common/Footer/Footer";
+import MainFees from "../components/features/Fees/MainFees";
+import Outreach from "../components/features/Fees/Outreach";
+import Packages from "../components/features/Fees/Packages";
 
 
 const Fees = () => {
     return (
-        <div>
-            <p>Fees</p>
+        <div className='flex flex-col '>
+            <MainFees />
+
+            <Packages />
+
+            <Outreach />
+
+            <Contact />
+
+            <Footer />
         </div>
     )
 }
