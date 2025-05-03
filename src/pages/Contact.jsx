045cde9955226fbd6854fe56217e7912de5c69f8
Ben Smerd <bsmerd@adam.com.au>
2025-05-03 20:06:57 +0930

    add in the contact page

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,9 +1,12 @@
 import Footer from "../components/common/Footer/Footer";
+import BookSession from "../components/features/Contact/BookSession";
+import ContactInfo from "../components/features/Contact/ContactInfo";
+import ContactSession from "../components/features/Contact/ContactSession";
 
 const Contact = () => {
     return (
-        <div>
-            <p>Cointact</p>
+        <div className='flex flex-col '>
+            <ContactSession />
 
             <Footer />
         </div>
