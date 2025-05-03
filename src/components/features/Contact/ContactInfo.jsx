c8b321a81b11a4d3e988efde9e4c4d3f63632aee
Ben Smerd <bsmerd@adam.com.au>
2025-05-03 20:07:15 +0930

    add in the contact info component on contact page

---
+++ src/components/features/Contact/ContactInfo.jsx
@@ -0,0 +1,12 @@
+const ContactInfo = () => {
+    return (
+        <div className='flex flex-col p-4 pb-12 bg-[#858D7E] text-slate-100'>
+            <div className='flex flex-col'>
+                <p className='font-cinzel text-lg'>Contact Novi Nest</p>
+                <a href="mailto:hello@novinestpsychology.com.au" className='underline'>hello@novinestpsychology.com.au</a>
+            </div>
+        </div>
+    )
+}
+
+export default ContactInfo;
