c28c00985509b9dfcb9f0e79c2b8546f10ff056e
Ben Smerd <bsmerd@adam.com.au>
2025-05-06 21:46:37 +0930

    update the a note on fees to be a button

--- src/pages/Services.jsx
+++ src/pages/Services.jsx
@@ -4,6 +4,7 @@ import AssessmentPackages from "../components/features/Services/AssessmentPackag
 import CognitiveAssessment from "../components/features/Services/CognitiveAssessment";
 import Expect from "../components/features/Services/Expect";
 import Contact from "../components/common/Contact/Contact";
+import { ArrowRightIcon } from '@heroicons/react/24/solid';
 
 const Services = () => {
     return (
@@ -29,9 +30,15 @@ const Services = () => {
                 <p className='text-sm'>We aim to complete the entire assessment process within 4 to 6 weeks. While we keep things moving behind the scenes, we also take our time where it matters most.</p>
             </div>
 
-            <div className='flex p-4 -mt-8'>
-                <Link to='/fees' >
-                    <p className='underline text-lg font-cinzel'>A Note on Fees</p>
+            <div className="flex p-4 -mt-8">
+                <Link
+                    to="/fees"
+                    className="flex items-center justify-between gap-2 w-fit px-4 py-2 rounded-lg bg-white shadow-sm hover:shadow-md hover:bg-gray-100 transition border border-gray-200 group"
+                >
+                    <p className="text-lg font-cinzel text-gray-800 group-hover:underline">
+                        A Note on Fees
+                    </p>
+                    <ArrowRightIcon className="h-5 w-5 text-gray-500 group-hover:text-gray-700 transition" />
                 </Link>
             </div>
 
