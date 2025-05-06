87524c92df11f05ec685c72fdae3e9a6ddbb78af
Ben Smerd <bsmerd@adam.com.au>
2025-05-06 20:49:17 +0930

    tidy up

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,6 +1,4 @@
 import Footer from "../components/common/Footer/Footer";
-import BookSession from "../components/features/Contact/BookSession";
-import ContactInfo from "../components/features/Contact/ContactInfo";
 import ContactSession from "../components/features/Contact/ContactSession";
 
 const Contact = () => {
