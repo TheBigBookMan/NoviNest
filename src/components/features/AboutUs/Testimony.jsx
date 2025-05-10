7b3d44963154002294d8529022d0e48fd14cf00f
Ben Smerd <bsmerd@adam.com.au>
2025-05-10 15:57:11 +0930

    update SEO for testimony in about us

--- src/components/features/AboutUs/Testimony.jsx
+++ src/components/features/AboutUs/Testimony.jsx
@@ -1,16 +1,16 @@
 const Testimony = () => {
     return (
-        <div className='flex flex-col bg-[#858D7E] p-4 text-slate-100 gap-8'>
+        <article id="testimony-article" aria-labelledby="testimony-heading" className='flex flex-col bg-[#858D7E] p-4 text-slate-100 gap-8'>
             <div className='flex flex-col items-center'>
-                <p className='text-2xl font-cinzel'>Ksenia's Approach</p>
+                <h3 id="testimony-heading" className='text-2xl font-cinzel'>Ksenia's Approach</h3>
             </div>
 
             <div className='flex flex-col items-center gap-4'>
-                <p className='font-playfair text-center text-lg'>"Lorem ipsum dolor sit amet consectetur, adipisicing elit. Maxime impedit non provident perspiciatis facilis adipisci molestiae asperiores, nostrum id."</p>
+                <blockquote className='font-playfair text-center text-lg'>"Lorem ipsum dolor sit amet consectetur, adipisicing elit. Maxime impedit non provident perspiciatis facilis adipisci molestiae asperiores, nostrum id."</blockquote>
                 
-                <p>- Ksenia Zinoviev</p>
+                <cite>- Ksenia Zinoviev</cite>
             </div>
-        </div>
+        </article>
     )
 }
 
