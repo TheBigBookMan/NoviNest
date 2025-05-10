2142f91cc40389fa40c9cf18ed2f89bd218da0a8
Ben Smerd <bsmerd@adam.com.au>
2025-05-10 16:11:34 +0930

    update SEO on contact page

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -3,11 +3,16 @@ import ContactSession from "../components/features/Contact/ContactSession";
 
 const Contact = () => {
     return (
-        <div className='flex flex-col '>
+        <section id="contact-page-section" aria-labelledby="contact-page-heading"  className='flex flex-col '>
+            <div className="flex flex-col gap-2 mb-4 p-6 leading-relaxed">
+                <h2 id="contact-page-heading" className="text-2xl font-cinzel">Book a Free Discovery Call</h2>
+                <p>Schedule a complimentary 15-minute phone consultation to explore how we can help.</p>
+            </div>
+
             <ContactSession />
 
             <Footer />
-        </div>
+        </section>
     )
 }
 
