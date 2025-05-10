fb545d9d4a8e9a058847087f3dd7fcbc2e23e9e2
Ben Smerd <bsmerd@adam.com.au>
2025-05-10 14:26:13 +0930

    update SEO differences

--- src/components/features/HomePage/Differences.jsx
+++ src/components/features/HomePage/Differences.jsx
@@ -21,16 +21,17 @@ const Differences = () => {
     const isInView = useInView(ref, { once: true, amount: 0.2 }); 
 
     return (
-        <motion.div
+        <motion.section
+            aria-labelledby="differences-heading"
             ref={ref}
             variants={containerVariants}
             initial="hidden"
             animate={isInView ? "visible" : "hidden"}
             className="flex flex-col h-fit p-4  pt-20 bg-[#F5F5F3] "
         >
-            <p className="text-3xl font-cinzel font-bold text-[#858D7E]">Our Differences</p>
+            <h2 className="text-3xl font-cinzel font-bold text-[#858D7E]">Our Differences</h2>
 
-            <div className="flex flex-col gap-6 mt-4 text-sm">
+            <ul className="flex flex-col gap-6 mt-4 text-sm">
                 {[
                 {
                     title: "Child-Centered Approach",
@@ -49,17 +50,17 @@ const Differences = () => {
                     description: "We partner with educators for your child's success.",
                 },
                 ].map((item, idx) => (
-                    <motion.div
+                    <motion.li
                         key={idx}
                         variants={itemVariants}
                         className="flex flex-col"
                     >
-                        <p className="italic text-[#B25D3E]">{item.title}</p>
+                        <h3 className="italic text-[#B25D3E]">{item.title}</h3>
                         <p className='text-[#4B5563] leading-relaxed'>{item.description}</p>
-                    </motion.div>
+                    </motion.li>
                 ))}
-            </div>
-        </motion.div>
+            </ul>
+        </motion.section>
     );
 };
 
