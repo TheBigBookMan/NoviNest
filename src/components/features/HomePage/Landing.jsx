112a1c47170b28bf8a8742e5d94dc8047c271e9f
Ben Smerd <bsmerd@adam.com.au>
2025-05-10 14:20:19 +0930

    update SEO landing page

--- src/components/features/HomePage/Landing.jsx
+++ src/components/features/HomePage/Landing.jsx
@@ -3,19 +3,19 @@ import { motion } from 'framer-motion';
 
 const Landing = () => {
     return (
-        <div className='flex flex-col h-screen justify-center items-center  p-4'>
+        <section  className='flex flex-col h-screen justify-center items-center  p-4' aria-labelledby="landing-section">
             <div className='flex flex-col gap-4 -mt-60 items-center'>
-                <img src={nestSVG} className='h-[220px] w-[280px]' />
+                <img src={nestSVG} alt="Novi Nest Psychology logo" className='h-[220px] w-[280px]' />
 
-                <motion.p
+                <motion.h1
                     initial={{opacity: 0, x: 20}}
                     animate={{opacity: 1, x: 0}}
                     transition={{duration: 1, ease: 'easeOut'}}
                     className='font-playfair text-3xl '>
                         Helping young minds flourish, grow with confidence and step into <i>brighter futures</i>
-                </motion.p>
+                </motion.h1>
             </div>
-        </div>
+        </section>
     )
 }
 
