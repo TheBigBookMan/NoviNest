cafb6cc2b483c4f59420852f231f04a94ff83170
Ben Smerd <bsmerd@adam.com.au>
2025-05-10 16:17:22 +0930

    update SEO main fees on fees page

--- src/components/features/Fees/MainFees.jsx
+++ src/components/features/Fees/MainFees.jsx
@@ -1,24 +1,14 @@
 const MainFees = () => {
     return (
-        <div className='flex flex-col gap-4  text-sm '>
-            <div className="flex flex-col gap-4 p-4">
-                <p className='text-2xl font-cinzel'>A Note on Fees</p>
-                
-                <p>At Novi Nest Psychology, our flat-rate assessment packages reflect the time, care, and clinical expertise that go into every stage of the process, from your first phone call to the personalised report and feedback session.</p>
+        <article id="main-fees-article" aria-labelledby="main-fees-heading" className='flex flex-col gap-2  text-sm bg-[#858D7E] text-slate-100 p-4 py-6 pb-12 leading-relaxed'>
+            <h3 id="main-fees-heading" className='text-xl'><span className='font-bold'>Hourly rate:</span> $240</h3>
 
-                <p>We believe in transparent pricing and strive to balance excellence with accessibility.</p>
-            </div>
+            <p className='italic'>(This is intentionally set below the Australian Psychological Society's recommended fee of $311 for a standard session, as outlined in the APS National Schedule of Suggested Fees 2024-2025. Our goal is to offer high-quality, specialist care while remaining mindful of affordability for families).</p>
 
-            <div className='flex flex-col gap-2 bg-[#858D7E] text-slate-100 p-4 pb-12'>
-                <p className='text-base'><span className='font-bold'>Hourly rate:</span> $240</p>
+            <p>To confirm an assessment booking, we kindly request a <span className='font-bold'>deposit of $240</span>, which covers the initial parent interview.</p>
 
-                <p className='italic'>(This is intentionally set below the Australian Psychological Society's recommended fee of $311 for a standard session, as outlined in the APS National Schedule of Suggested Fees 2024-2025. Our goal is to offer high-quality, specialist care while remaining mindful of affordability for families).</p>
-
-                <p>To confirm an assessment booking, we kindly request a <span className='font-bold'>deposit of $240</span>, which covers the initial parent interview.</p>
-
-                <p>Payment plans are available. Please note that final reports will only be provided once full payment has been received.</p>
-            </div>
-        </div>
+            <p>Payment plans are available. Please note that final reports will only be provided once full payment has been received.</p>
+        </article>
     )
 }
 
