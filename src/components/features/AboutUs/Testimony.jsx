ae7857a23d75e8a762629b15d6cdc8ad8f680240
Ben Smerd <bsmerd@adam.com.au>
2025-05-17 09:47:55 +0930

    update styling on images and video

--- src/components/features/AboutUs/Testimony.jsx
+++ src/components/features/AboutUs/Testimony.jsx
@@ -7,11 +7,11 @@ const Testimony = () => {
                 Ksenia's Approach
             </h2>
 
-            <div className="relative w-full mb-16 bg-[#858D7E] text-slate-100 shadow-xl overflow-hidden ">
+            <div className="relative w-full mb-16 bg-[#858D7E] text-slate-100 rounded-2xl shadow-xl overflow-hidden ">
                 <video
                     controls
                     preload="none"
-                    className="w-full h-auto"
+                    className="w-full h-auto rounded-2xl shadow-xl"
                 >
                     <source src={kseniaApproach} type="video/mp4" />
                     Your browser does not support the video tag.
@@ -20,7 +20,7 @@ const Testimony = () => {
 
             <article 
                 aria-labelledby="testimony-heading" 
-                className="flex flex-col bg-[#F5F5F3] p-8 text-[#333] gap-8 shadow-md border-l-4 border-[#B25D3E] max-w-3xl w-full"
+                className="flex flex-col bg-[#F5F5F3] p-8 text-[#333] gap-8 rounded-2xl shadow-xl border-l-4 border-[#B25D3E] max-w-3xl w-full"
             >
                 <blockquote className="font-playfair text-center text-xl text-[#B25D3E] leading-relaxed mb-4">
                     “Ultimately, this work is about helping things make sense, so that moving forward feels a little less overwhelming, and a little more possible.”
